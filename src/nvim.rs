dc1bf55a46927329e0b30db363c9dc7d0b9d058f
daa <andrey.dubravin@gmail.com>
2016-03-19 14:32:59 +0300

    Fix embed mode

--- src/nvim.rs
+++ src/nvim.rs
@@ -1,4 +1,4 @@
-use neovim_lib::{Neovim, Session};
+use neovim_lib::{Neovim, NeovimApi, Session};
 use std::io::{Result, Error, ErrorKind};
 use rmp::Value;
 
@@ -8,12 +8,14 @@ pub struct Nvim {
 
 impl Nvim {
     pub fn start() -> Result<Nvim> {
-        let mut session = try!(Session::new_tcp("127.0.0.1:6666"));
-        //let mut session = try!(Session::new_child());
+        //let mut session = try!(Session::new_tcp("127.0.0.1:6666"));
+        let mut session = try!(Session::new_child());
 
         session.start_event_loop_cb(Nvim::cb);
 
         let mut nvim = Neovim::new(session);
+        // fix neovim --embed bug to start embed mode
+        nvim.input("i").unwrap();
         try!(nvim.ui_attach(80, 24, true).map_err(|e| Error::new(ErrorKind::Other, e)));
 
         Ok(Nvim {
