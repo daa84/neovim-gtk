f337b63f1eeb7554c60921a5cc46916f871a2eb0
daa84 <daa84@inbox.ru>
2017-03-22 18:37:34 +0300

    Thoughts

--- src/cursor.rs
+++ src/cursor.rs
@@ -2,6 +2,9 @@ use cairo;
 use ui_model::Color;
 use shell::{Shell, NvimMode};
 
+use glib;
+
+// display, 2 sec -> hiding 1 sec -> not visible 1 sec -> showing 1 sec
 pub struct Cursor {
 }
 
@@ -28,4 +31,30 @@ impl Cursor {
         ctx.rectangle(current_point.0, line_y, cursor_width, line_height);
         ctx.fill();
     }
+}
+
+
+pub struct Animation {
+    state_stream: Vec<Box<AnimationState>>,
+    state: Option<Box<AnimationState>>,
+    timer: Option<glib::SourceId>,
+}
+
+impl Animation {
+    pub fn new() -> Animation {
+        Animation { 
+            state_stream: vec![],
+            state: None,
+            timer: None,
+        }
+    }
+}
+
+trait AnimationState {
+    fn clone(&self) -> AnimationState;
+
+    // [TODO]: Description - repaint rect here
+    fn next(&mut self) -> Option<u32>;
+
+    fn paint(&self, ctx: &cairo::Context, shell: &Shell);
 }
